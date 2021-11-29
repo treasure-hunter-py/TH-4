65e7a665bd47015ca6c84c1af4bd1360638c08bf
treasure-hunter-py <94384053+treasure-hunter-py@users.noreply.github.com>
2021-11-29 18:59:36 +0200

    Update home-work-1.py

--- home-work-1.py
+++ home-work-1.py
@@ -1,8 +1,10 @@
-''' 1 1. Написати функцію < square > , яка прийматиме один аргумент - сторону квадрата, і вертатиме 3 значення (кортеж): периметр квадрата, площа квадрата та його діагональ. '''
+''' 1 1. Написати функцію < square > , 
+яка прийматиме один аргумент - сторону квадрата, і вертатиме 3 значення (кортеж):
+ периметр квадрата, площа квадрата та його діагональ. '''
 
 def square(edge_rectangle):
     import math
-    character_rectangle=(edge_rectangle*4, edge_rectangle*2, math.sqrt(2)*edge_rectangle)
+    character_rectangle=(edge_rectangle*4, edge_rectangle*edge_rectangle, math.sqrt(2)*edge_rectangle)
     return character_rectangle
 
 print(square(float(input('enter edge rectangle '))))
