7cac6aed5d870850988474cdb9af2a47da5efc66
treasure-hunter-py <94384053+treasure-hunter-py@users.noreply.github.com>
2021-11-29 16:46:09 +0200

    Create home-work-4.py

---
+++ home-work-4.py
@@ -0,0 +1,22 @@
+'''4. Написати функцію < prime_list >, яка прийматиме 2 аргументи - початок і кінець діапазона, і вертатиме список простих чисел всередині цього діапазона.'''
+
+def prime_list(start_pul, finish_pul):
+
+    def is_prime(number):
+        temp = True
+        for num_checked in range(1, number):
+            if num_checked in [1, number]:
+                continue
+            if number % num_checked == 0:
+                temp = False
+                break
+            else:
+                temp = True
+        return temp
+
+    for i in range (start_pul,finish_pul+1):
+        print(f'{i} = {is_prime(i)}')
+
+start = int(input('enter start position '))
+finish = int(input('enter start position '))
+prime_list(start, finish)
